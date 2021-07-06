053aae265003285c47efc5b46bcab2edffd34016
Chetan Mandur <ChetanMandur45@gmail.com>
2021-07-06 17:19:00 -0400

    changed a tag to link in errorpage

--- src/Base/PageNotFound.js
+++ src/Base/PageNotFound.js
@@ -1,10 +1,10 @@
 import './css/PageNotFound.css';
-
+import {Link } from "react-router-dom";
 const PageNotFound = () => {
     return (
         <div className="pagenotfound">
             <div className="errorTitle">Page does not exist!</div>
-            <div className="errorText">Most likely a broken link... <a href="/cook">Click here to go back</a></div>
+            <div className="errorText">Most likely a broken link... <Link to="/cook">Click here to go back</Link></div>
         </div>
     );
 }
