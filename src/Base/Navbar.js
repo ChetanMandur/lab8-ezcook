670cbdd9249843b250dad823be68247ecfbaeb89
Chetan Mandur <ChetanMandur45@gmail.com>
2021-07-06 18:52:21 -0400

    changes nav links back to a tag

--- src/Base/Navbar.js
+++ src/Base/Navbar.js
@@ -1,16 +1,16 @@
 import './css/Navbar.css'
 import {Link } from "react-router-dom";
+
+
 const Navbar = () => {
     return ( 
         <nav className="navbar">
             <h1><span style={{color:'orange', fontWeight:'bold'}}>EZ</span>cook</h1>
             <div className="links">
-                {/* <a href={"/cook"}>Cook</a>
-                <a href={"/learn"}>Learn</a>
-                <a href={"/create"}>Create</a> */}
-                <Link to="/cook">Cook</Link>
-                <Link to="/learn">Learn</Link>
-                <Link to="/create">Create</Link>
+
+                <a href="#/cook">Cook</a>
+                <a href="#/learn">Learn</a>
+                <a href="#/create">Create</a>
             </div>
         </nav>
     );
