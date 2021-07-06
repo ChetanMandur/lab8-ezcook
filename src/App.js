23cc4f7ac360a4a71168ae4ef4c428d9e8c615ee
Chetan Mandur <ChetanMandur45@gmail.com>
2021-07-06 17:24:15 -0400

    edited link formatting

--- src/App.js
+++ src/App.js
@@ -8,39 +8,8 @@ import {HashRouter as Router, Route, Switch} from 'react-router-dom';
 
 
 function App() {
-  // return (
-  //   <Router basename={"/lab8-ezcook"}>
-  //     <div className="App">
-  //       <Navbar/> 
-  //       <div className="content">
-  //         <Switch>
-  //           <Route exact path= { "/cook"}>
-  //             <Cook></Cook>
-  //           </Route>
-            
-
-  //           <Route exact path={"/learn"}>
-  //             <Learn></Learn>
-  //           </Route>
-
-  //           <Route exact path={"/"}>
-  //             <Cook></Cook>
-  //           </Route>
-
-  //           <Route path="*">
-  //             <PageNotFound></PageNotFound>
-  //           </Route>
-
-
-  //         </Switch>
-          
-  //       </div>
-  //     </div>
-  //   </Router>
-  // );
-
   return (
-      <Router basename={process.env.PUBLIC_URL}>
+      <Router >
         <div className="App">
           <Navbar/> 
           <div className="content">
