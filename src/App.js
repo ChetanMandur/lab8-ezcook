1bf7c5e47b3100a5e097ea76aa5cc9ccacdc3ee6
Chetan Mandur <ChetanMandur45@gmail.com>
2021-07-06 17:14:22 -0400

    hashrouter

--- src/App.js
+++ src/App.js
@@ -4,61 +4,68 @@ import Learn from './Learn/Learn';
 
 import PageNotFound from './Base/PageNotFound';
 
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {HashRouter as Router, Route, Switch} from 'react-router-dom';
 
-// import { HashRouter as Router } from 'react-router-dom';
-// import {BrowserRouter as  Route, Switch} from 'react-router-dom';
-
-// import {HashRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
-  return (
-    <Router basename={"/lab8-ezcook"}>
-      <div className="App">
-        <Navbar/> 
-        <div className="content">
-          <Switch>
-            <Route exact path= { "/cook"}>
-              <Cook></Cook>
-            </Route>
-            
-
-            <Route exact path={"/learn"}>
-              <Learn></Learn>
-            </Route>
-
-            <Route exact path={"/"}>
-              <Cook></Cook>
-            </Route>
-
-            <Route path="*">
-              <PageNotFound></PageNotFound>
-            </Route>
-
-
-          </Switch>
-          
-        </div>
-      </div>
-    </Router>
-  );
-
   // return (
-  //   <Router basename={process.env.PUBLIC_URL}>
+  //   <Router basename={"/lab8-ezcook"}>
   //     <div className="App">
   //       <Navbar/> 
   //       <div className="content">
   //         <Switch>
-  //           <Route exact path={process.env.PUBLIC_URL + "/"} component={Cook}></Route>
-  //           <Route exact path={process.env.PUBLIC_URL + "/cook"} component={Cook}></Route>
-  //           <Route exact path={process.env.PUBLIC_URL + "/learn"} component={Learn}></Route>
-  //           <Route component={PageNotFound} status={404}></Route>
+  //           <Route exact path= { "/cook"}>
+  //             <Cook></Cook>
+  //           </Route>
+            
+
+  //           <Route exact path={"/learn"}>
+  //             <Learn></Learn>
+  //           </Route>
+
+  //           <Route exact path={"/"}>
+  //             <Cook></Cook>
+  //           </Route>
+
+  //           <Route path="*">
+  //             <PageNotFound></PageNotFound>
+  //           </Route>
+
+
   //         </Switch>
           
   //       </div>
   //     </div>
   //   </Router>
   // );
+
+  return (
+      <Router basename={process.env.PUBLIC_URL}>
+        <div className="App">
+          <Navbar/> 
+          <div className="content">
+            <Switch>
+              <Route exact path="/cook">
+                  <Cook />
+                </Route>
+                <Route exact path="/learn">
+                  <Learn />
+                </Route>
+                <Route exact path="/">
+                  <Cook />
+                </Route>
+                <Route path="*">
+                  <PageNotFound />
+                </Route>
+
+            </Switch>
+            
+          </div>
+        </div>
+      </Router>
+
+
+  );
 
 
 }
--- src/Base/Navbar.js
+++ src/Base/Navbar.js
@@ -1,12 +1,16 @@
 import './css/Navbar.css'
+import {Link } from "react-router-dom";
 const Navbar = () => {
     return ( 
         <nav className="navbar">
             <h1><span style={{color:'orange', fontWeight:'bold'}}>EZ</span>cook</h1>
             <div className="links">
-                <a href={"/cook"}>Cook</a>
+                {/* <a href={"/cook"}>Cook</a>
                 <a href={"/learn"}>Learn</a>
-                <a href={"/create"}>Create</a>
+                <a href={"/create"}>Create</a> */}
+                <Link to="/cook">Cook</Link>
+                <Link to="/learn">Learn</Link>
+                <Link to="/create">Create</Link>
             </div>
         </nav>
     );
