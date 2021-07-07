07c078cd7a327e9845acf0bbcba824fd8b2ba2cb
Chetan Mandur <ChetanMandur45@gmail.com>
2021-07-06 22:33:59 -0400

    added animation to button on navbar

--- src/Base/Navbar.js
+++ src/Base/Navbar.js
@@ -1,6 +1,26 @@
 import './css/Navbar.css'
-import {Link } from "react-router-dom";
 
+function colourChangeClick(id) {
+    document.getElementById(id).style.backgroundColor = "white";
+    const navButtonIDList = ["cookNav", "learnNav", "createNav"];
+    for (let currentID of navButtonIDList){
+        document.getElementById(currentID).style.color = "black";
+    }
+    document.getElementById(id).style.color = "orange";
+    
+}
+
+function colourChangeHover(id){
+    var currentColor = document.getElementById(id).style.color;
+    if (currentColor !== "orange"){
+        document.getElementById(id).style.backgroundColor = "orange";
+    }
+
+}
+
+function colourChangeOff(id){
+    document.getElementById(id).style.backgroundColor = "white";  
+}
 
 const Navbar = () => {
     return ( 
@@ -8,9 +28,23 @@ const Navbar = () => {
             <h1><span style={{color:'orange', fontWeight:'bold'}}>EZ</span>cook</h1>
             <div className="links">
 
-                <a href="#/cook">Cook</a>
-                <a href="#/learn">Learn</a>
-                <a href="#/create">Create</a>
+                <a href="#/cook" id="cookNav" style={{color:"orange"}} 
+                    onClick={(e) => colourChangeClick(e.target.id)}
+                    onMouseOver={(e) => colourChangeHover(e.target.id)}
+                    onMouseOut={(e) => colourChangeOff(e.target.id) }
+                    >Cook</a>
+
+                <a href="#/learn" id="learnNav" 
+                    onClick={(e) => colourChangeClick(e.target.id)}
+                    onMouseOver={(e) => colourChangeHover(e.target.id)}
+                    onMouseOut={(e) => colourChangeOff(e.target.id)}
+                >Learn</a>
+
+                <a href="#/create" id="createNav" 
+                    onClick={(e) => colourChangeClick(e.target.id)}
+                    onMouseOver={(e) => colourChangeHover(e.target.id)}
+                    onMouseOut={(e) => colourChangeOff(e.target.id)}
+                >Create</a>
             </div>
         </nav>
     );
--- src/Cook/Cook.js
+++ src/Cook/Cook.js
@@ -5,5 +5,6 @@ const Cook = () => {
         </div>
     );
 }
+
  
 export default Cook;
